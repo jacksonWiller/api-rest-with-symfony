1b75819279d3024ab691d3556278fe99d9e09216
Jackson Willer Duarte <jackson.duarte@uloc.dev>
2020-11-22 04:52:10 -0300

    controller rest

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -11,13 +11,28 @@ use Symfony\Component\HttpFoundation\Request;
 class ProductController extends AbstractController
 {
     /**
-     * @Route("/product", name="product")
+     * @Route("/product", name="show_products", methods={"GET"})
      */
-    public function index(): Response
+    public function index()
     {
+        $products = $this->getDoctrine()->getRepository(Product::class)->findAll();
+
+
         return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/ProductController.php',
+            'data' => $products
+        ]);
+    }
+
+    /**
+     * @Route("/product/{productId}", name="show_products", methods={"GET"})
+     */
+    public function show($productId)
+    {
+        $product = $this->getDoctrine()->getRepository(Product::class)->find($productId);
+
+
+        return $this->json([
+            'data' => $product
         ]);
     }
 
@@ -49,6 +64,59 @@ class ProductController extends AbstractController
 
         return $this->json([
             'message' => 'Produto criado com sucesso!',
+        ]);
+    }
+
+
+    /**
+     * @Route("/product", name="update_product", methods={"PUT"})
+     */
+    public function update(Request $resquest)
+    {
+
+        $date = new \DateTime('@'.strtotime('now'));
+        $date->format('Y-m-d H:i:s');
+
+        $productData = $resquest->request->all();
+
+        $product = new Product();
+        $product->setName($productData['name']);
+        $product->setDescription($productData['description']);
+        $product->setContent($productData['content']);
+        $product->setPrice($productData['price']);
+        $product->setSlug($productData['slug']);
+        $product->setIsActive(true);
+        $product->setCreatedAt($date);
+        $product->setUpdatedAt($date);
+        
+        $doctrine = $this->getDoctrine()->getManager();
+
+        $doctrine->persist($product);
+        $doctrine->flush();
+
+        return $this->json([
+            'message' => 'Produto criado com sucesso!',
+        ]);
+    }
+
+
+    /**
+     * @Route("/product/{productId}", name="delete_product", methods={"DELETE"})
+     */
+    public function remove($productId)
+    {
+
+        $doctrine = $this->getDoctrine();
+
+        $product = $doctrine->getRepository(Product::class)->find($productId);
+        
+        $doctrine = $this->getDoctrine()->getManager();
+
+        $doctrine->persist($product);
+        $doctrine->flush();
+
+        return $this->json([
+            'message' => 'Produto deletado com sucesso!',
         ]);
     }
 
